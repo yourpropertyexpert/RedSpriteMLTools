de5047f95015880d016d12c4fc2ac18d77a2f9db
Mark Harrison <yourpropertyexpert@gmail.com>
2023-06-16 12:29:17 +0100

    Now with tests from GPT3

---
+++ src/tests/unit/SampleChatGPTTest.php
@@ -0,0 +1,34 @@
+<?php
+// File: DemoTest.php
+
+use PHPUnit\Framework\TestCase;
+use redspritedemo\Demo;
+
+class DemoTest extends TestCase
+{
+    public function testGetResults()
+    {
+        $demo = new Demo([2, 4, 6]);
+        $expectedResult = '{"inputvalues":[2,4,6],"sum":12,"count":3,"average":4}';
+        $this->assertEquals($expectedResult, $demo->getResults());
+    }
+
+    public function testGetSum()
+    {
+        $demo = new Demo([2, 4, 6]);
+        $this->assertEquals(12, $demo->getSum());
+    }
+
+    public function testGetCount()
+    {
+        $demo = new Demo([2, 4, 6]);
+        $this->assertEquals(3, $demo->getCount());
+    }
+
+    public function testGetAverage()
+    {
+        $demo = new Demo([2, 4, 6]);
+        $this->assertEquals(4, $demo->getAverage());
+    }
+}
+?>
